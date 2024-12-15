75cb0c322673098151554997e8042c47e6ce3661
Zeeshan Islam <159948421+Rohaan001@users.noreply.github.com>
2024-12-15 20:29:38 +0500

    add

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-// console.log("test");
+
 
 var weather = document.getElementById("weather");
 var city = document.getElementById("city");
 
- function getWeather() {
+function getWeather() {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=392fc470c1ac8b42b2f40951a9a96cc4&units=metric`
   )
@@ -15,17 +15,31 @@ var city = document.getElementById("city");
 
 
       console.log(data.weather[0].main);
-      // console.log(data.weather[0].des);
+  
 
+      const weatherCondition = data.weather[0].main.toLowerCase();
+      console.log(weatherCondition);
+      const weatherImages = {
+        clear: "images/sun.png",
+        clouds: "images/clouds.png",
+        rain: "images/rain.png",
+        snow: "images/snow.png",
+        thunderstorm: "images/bolt.png",
+        drizzle: "images/drizzle.png",
+        haze: "images/fog.png",
+        default: "images/default_weather.png",
+      };
 
+      const weatherImage = weatherImages[weatherCondition] || weatherImages.default;
 
 
       weather.innerHTML = `
     <h1 class="temp">${Math.round(data.main.temp)}°C</h1>
     <h2>${data.name}</h2>
+    <img src="${weatherImage}" alt="${data.weather[0].main}" class="weather-icon">
     <h3>${data.weather[0].main}</h3>
     <h3>${data.wind.speed} km/h</h3>
-    <h3>${data.main.humidity}%</h3>
+    <h3>Humidity: ${data.main.humidity}%</h3>
     `;
     })
     .catch(function (err) {
@@ -33,4 +47,8 @@ var city = document.getElementById("city");
     });
 }
 
-// getWeather()
+function checkEnter(event) {
+  if (event.key === "Enter") {
+    getWeather();
+  }
+}
